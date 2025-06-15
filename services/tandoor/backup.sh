8ae59a5cd126eae0081494adf1e587cbbe696801
Joost Zöllner <joost.zoellner@holisticon.de>
2025-06-15 15:06:14 +0200

    tandoor

--- services/tandoor/backup.sh
+++
@@ -1,5 +0,0 @@
-BACKUPDIR="$BACKUP_BASE_DIR/tandoor"
-mkdir -p "$BACKUPDIR"
-
-info "Tandoor - Backup DB"
-docker compose exec db_recipes pg_dump -U djangouser djangodb > "$BACKUPDIR/db.sql"
--- services/tandoor/restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BACKUPDIR="$BACKUP_BASE_DIR/tandoor"
-
-info "Tandoor - Restore DB"
-docker compose exec -T db_recipes psql -X -U djangouser -d djangodb <  "$BACKUPDIR/db.sql"

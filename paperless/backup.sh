5696ee175fe3bdcd8de1d9a14cbc1a93fae1ddc2
Joost Zöllner <github@joost.io>
2025-06-15 18:07:25 +0200

    cleanup

--- paperless/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p backup
-
-info "Paperless - Backup Documents"
-docker-compose exec -T webserver document_exporter ../export
-cp -R ./export ./backup
--- paperless/restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-info "Paperless - Restore Documents"
-cp -R ./backup/* ./export
-docker-compose exec -T webserver document_importer ../export

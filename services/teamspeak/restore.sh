013c01577e01e454abab15b6d11462edcc609e12
Joost Zöllner <joost@zoellner.sh>
2025-10-14 12:08:32 +0200

    Update restore.sh

--- services/teamspeak/restore.sh
+++ services/teamspeak/restore.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-BACKUPDIR="$BACKUP_BASE_DIR/teamspeak"
-
-info "Teamspeak - Restore DB"
-docker compose exec -T db mariadb -u root -p"changeme" < "$BACKUPDIR/db.sql"
+docker compose exec -T db mariadb -u root -p"changeme" < db.sql

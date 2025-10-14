51c9ab9b391952c03b164e5f9c4627278af07bd5
Joost Zöllner <joost@zoellner.sh>
2025-10-14 11:33:07 +0200

    Update backup.sh

--- services/teamspeak/backup.sh
+++ services/teamspeak/backup.sh
@@ -1,6 +1,2 @@
-BACKUPDIR="$BACKUP_BASE_DIR/teamspeak"
-mkdir -p "$BACKUPDIR"
-
-info "Teamspeak - Backup DB"
 rm -rf ./data/mariadb/backup
-docker compose exec db mariadb-dump --all-databases -u root -p"changeme" > "$BACKUPDIR/db.sql"
+docker compose exec db mariadb-dump --all-databases -u root -p"changeme" > db.sql
